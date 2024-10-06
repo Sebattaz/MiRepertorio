a6f4f568a2d9be440e32ed80be0dfb45476586c5
Sebattaz <seba.lostar@gmail.com>
2024-10-06 18:56:37 -0300

    Modificaciones en index

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 
-// Middleware para permitir recibir datos en formato JSON
+// Middleware
 app.use(express.json());
 
 app.get("/", (req, res) => { res.sendFile(path.join(__dirname, './', 'index.html'))});
 
-// Función para leer el archivo repertorio.json
+
 const leerRepertorio = () => {
     const data = fs.readFileSync('repertorio.json', 'utf-8');
     return JSON.parse(data);
 };
 
-// Función para escribir en el archivo repertorio.json
+// Funcion para escribir en el archivo
 const escribirRepertorio = (data) => {
     fs.writeFileSync('repertorio.json', JSON.stringify(data, null, 2), 'utf-8');
 };
 
-// POST /canciones: Agregar una canción al repertorio
+//Agregar una canción al repertorio
 app.post('/canciones', (req, res) => {
     const repertorio = leerRepertorio();
     const nuevaCancion = req.body;
@@ -28,13 +28,13 @@ app.post('/canciones', (req, res) => {
     res.status(201).send('Canción agregada con éxito');
 });
 
-//GET /canciones: Listar todas las canciones
+//Listar todas las canciones
 app.get('/canciones', (req, res) => {
     const repertorio = leerRepertorio();
     res.json(repertorio);
 });
 
-// PUT /canciones/:id: Actualizar una canción
+//Actualizar una cancion
 app.put('/canciones/:id', (req, res) => {
     const repertorio = leerRepertorio();
     const { id } = req.params;
@@ -49,7 +49,7 @@ app.put('/canciones/:id', (req, res) => {
     }
 });
 
-// DELETE /canciones/:id: Eliminar una canción
+//Eliminar una cancion
 app.delete('/canciones/:id', (req, res) => {
     const repertorio = leerRepertorio();
     const { id } = req.params;
@@ -57,15 +57,15 @@ app.delete('/canciones/:id', (req, res) => {
 
     if (repertorio.length !== nuevasCanciones.length) {
         escribirRepertorio(nuevasCanciones);
-        res.send('Canción eliminada con éxito');
+        res.send('Cancion eliminada con éxito');
     } else {
-        res.status(404).send('Canción no encontrada');
+        res.status(404).send('Cancion no encontrada');
     }
 });
 
-// Iniciar el servidor en el puerto 3000
+// Iniciar el servidor
 app.listen(3000, () => {
-    console.log('Servidor escuchando en http://localhost:3000');
+    console.log('Servidor levantado');
 });
 
 
